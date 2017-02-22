f91573b2269aae2f8d5b236a3b8975d00b45e2f6
Nikita Kastyshyn <4luckynikita@gmail.com>
2017-02-21 18:08:28 -0800

    ss

---
+++ module4-solution/SpeakGoodBye.js
@@ -0,0 +1,12 @@
+(function () {
+
+var byeSpeaker = speakG;
+
+
+var speakWordG = "Good Bye";
+
+function speakG(name) {
+  console.log(speakWordG + " " + name);
+}
+
+});
---
+++ module4-solution/SpeakHello.js
@@ -0,0 +1,18 @@
+console.log('test');
+var helloSpeaker =  speak;
+
+var speakWord = "Hello";
+
+
+function speak(name) {
+  console.log(speakWord + " " + name);
+}
+helloSpeaker("test");
+
+/*
+var speakWord = "Hello";
+
+function speak(name) {
+  console.log(speakWord + " " + name);
+}
+*/
